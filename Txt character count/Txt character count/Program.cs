00f8cf95be4b21c88238236169b8049bd2d8a365
Palpatin-lll <117033246+Palpatin-lll@users.noreply.github.com>
2023-01-28 14:26:49 +0300

    Txt character count
    This program counts the number of words in this file.

---
+++ Txt character count/Txt character count/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+
+namespace Txt_character_count
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Count = "";
+            string[] txt;
+            StreamReader sr = new StreamReader("file.txt", System.Text.Encoding.Default); //File location: Txt character count\bin\Debug\file.txt
+            Console.OutputEncoding = Encoding.GetEncoding(866);
+            while(sr.EndOfStream != true) { Count = sr.ReadLine();}
+            txt = Count.Split(' ');
+            Console.WriteLine("Characters: ");
+            Console.WriteLine(txt.Length);
+            sr.Close();
+            Console.ReadLine();
+        }
+    }
+}
